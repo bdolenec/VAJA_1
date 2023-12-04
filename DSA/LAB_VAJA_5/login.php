3bbd67345cd475feece4a4292d3a9f48f7580fcc
bdolenec <bd7494@student.uni-lj.si>
2023-12-04 11:08:48 +0100

    4/12/23

---
+++ DSA/LAB_VAJA_5/login.php
@@ -0,0 +1,28 @@
+<?php
+        session_start();
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H1>LOGIN</H1>
+
+    <div style="text-align: center;">
+    <form action="/index.php" method="POST">
+ 
+       <input type="text" name="User" id="" placeholder="Username">
+       <input type="password" name="Pass" id="" placehoder="Password">
+       <button type="submit">Submit</button>
+ 
+    </form>    
+    </div>
+ 
+ 
+ 
+</body>
+</html>
